601673342ec7219698a9a0b3350ec69e52ced9d9
luffycheung <zhd172@gmail.com>
2014-04-07 02:50:43 +0800

    update

---
+++ js/myplaylist.js
@@ -0,0 +1,112 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'http://l.a.ppsrc.com/2014/0308/15/lkIRSVopyc7HrOczY7uuTgXZgf_3pm',
+        title:'千本桜(Dubstep remix)',
+        artist:'初音ミク',
+        rating:4,
+        buy:'http://papa.me/post/K8V5b9Jf',
+        price:'papa',
+        duration:'04:32',
+        cover:'http://m2.img.srcdd.com/farm4/d/2014/0308/15/A66EBFC10DEF7492EBDC83B2F467D06E_LARGE_230_230.jpeg'
+    },
+     {
+        mp3:'http://l.a.ppsrc.com/2014/0325/22/PR4_-bgyi0XHrOczY7uuTgghOK_3pm',
+        title:'DECORATOR (TeddyLoid remix)',
+        artist:'livetune feat. Hatsune Miku',
+        rating:3,
+        buy:'http://papa.me/post/FG5ib0xf',
+        price:'papa',
+        duration:'05:34',
+        cover:'http://m.i.ppsrc.com/cT1neKoI_DLHFCEJTB-wgQ/w-640_h-640_t-webp'
+
+    },
+    {
+        mp3:'http://l.a.ppsrc.com/2014/0323/23/SalYrPaDABnHrOczY7uuTg6aOc_3pm',
+        title:'Awake',
+        artist:'Tycho',
+        rating:3,
+        buy:'http://papa.me/post/Ku1c5zE8',
+        price:'papa',
+        duration:'04:44',
+        cover:'http://m.i.ppsrc.com/c5uO1V-H5FHHFCEJTB-wgQ/w-640_h-640_t-webp'
+
+    },
+     {
+        mp3:'http://l.a.ppsrc.com/2014/0323/22/VtEB5RXlFr3HrOczY7uuTgeK2g_3pm',
+        title:'4',
+        artist:'Aphex Twin',
+        rating:3,
+        buy:'http://papa.me/album/xN0iLGtxjzap_3QhcYrGnw',
+        price:'papa',
+        duration:'03:38',
+        cover:'http://m.i.ppsrc.com/k0w95tBoS5THFCEJTB-wgQ/w-640_h-640_t-webp'
+
+    },
+    
+     {
+        mp3:'http://a.srcdd.com/farm1/255/F80370804839113466634CC495AED3FF.mp3',
+        title:'Out of the Ashes',
+        artist:'Selectracks',
+        rating:3,
+        buy:'http://www.diandian.com/tag/Selectracks',
+        price:'dd',
+        duration:'02:21',
+        cover:'http://m1.img.srcdd.com/farm3/d/2012/0224/16/E0DB890FC83AFD8D0F65E83A2900A7F3_B500_900_500_312.JPEG'
+
+    },
+     {
+        mp3:'http://a.srcdd.com/farm1/245/E27A8451F96BFB496D7677D8AAD314F5.mp3',
+        title:'1-22 필살의 검, Part 2',
+        artist:'剑灵 OST',
+        rating:3,
+        buy:'http://ostfm.diandian.com/post/2013-12-09/40060382872',
+        price:'dd',
+        duration:'02:28',
+        cover:'http://m1.img.srcdd.com/farm5/d/2013/1209/20/7F8503DC8EFD86D9B204F34E942BEDCB_S250_250_250.jpeg'
+
+   },
+   {
+        mp3:'http://a.srcdd.com/farm1/0/8A694215A9E28EDA2B49C21E336CA500.mp3',
+        title:'Cut Your Teeth (Kygo Remix)',
+        artist:'Kyla La Grange',
+        rating:3,
+        buy:'http://d-luffy.diandian.com/post/2014-03-02/40061124187',
+        price:'dd',
+        duration:'06:36',
+        cover:'http://m3.img.srcdd.com/farm4/d/2014/0302/17/BF10D7CA8E52D2B8A6951D2BF92948AA_B1280_1280_500_500.jpeg'
+
+    },
+     {
+        mp3:'http://l.a.ppsrc.com/2014/0308/14/kboVV_vih__HrOczY7uuTgY66s_3pm',
+        title:'Power Glove',
+        artist:'Knife Party',
+        rating:3,
+        buy:'http://papa.me/post/zm7oC3N3',
+        price:'papa',
+        duration:'04:23',
+        cover:'http://m2.img.srcdd.com/farm4/d/2014/0308/15/28E56482BC285CBFDDE8EB0ACAA53D67_LARGE_230_230.jpeg'
+
+    },
+     {
+        mp3:'http://a.srcdd.com/farm1/65/AE1696DC609B5F09A77CEADCB4B0D841.mp3',
+        title:'Belispeak',
+        artist:'Purity Ring',
+        rating:3,
+        buy:'http://d-luffy.diandian.com/post/2012-12-09/Chillwave-Purity_Ring-Belispeak',
+        price:'dd',
+        duration:'02:58',
+        cover:'http://m3.img.srcdd.com/farm5/d/2012/1209/21/A58C9B543A49C69FD5B5FE05E027E205_B500_900_500_500.JPEG'
+
+     }
+  
+  
+];
